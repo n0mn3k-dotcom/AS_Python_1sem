b9af2ebd88e0bae11733464840a42cdd8062a00e
0y0y0y <n0mn3k@gmail.com>
2025-10-14 16:11:43 +0300

    Update main.py

--- 1.1lab/main.py
+++ 1.1lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+# Задание 6 из ЕГЭ-2025 профиль [https://math-ege.sdamgia.ru/problem?id=283063]
+# Найдите корень уравнения (x+8)**5 = 243
+
+import math
+
+num = 243
+
+root = num ** (1/5)
+
+x = root - 8
+print('Ответ:', x)
