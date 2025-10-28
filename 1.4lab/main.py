c2e7fcceff16083374abab291e71d88e46c8c1be
0y0y0y <n0mn3k@gmail.com>
2025-10-28 16:30:59 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+numbers = [3, 7, 2, 8, 5, 1, 4, 6]
+
+
+for i in range(len(numbers)):
+    if numbers[i] % 2 == 0:
+        print(numbers[i], end='')
+
+print()
+
+for i in range(len(numbers)-1, -1, -1):
+    if numbers[i] % 2 == 1:
+        print(numbers[i], end='')
