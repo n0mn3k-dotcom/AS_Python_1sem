9348dba05cbfdc0bb10be395283a4945ff6ebeca
0y0y0y <n0mn3k@gmail.com>
2025-11-04 22:39:32 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+str_1 = '41242121Abc'
+str_2 = '975293573JkRs'
+
+new_str_1 = str_2[:2] + str_1[2:]
+new_str_2 = str_1[:2] + str_2[2:]
+
+print(new_str_1 + ' ' + new_str_2)
+
