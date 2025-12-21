a1ef20f452ec338ba618b41f0e405c4161af900a
0y0y0y <n0mn3k@gmail.com>
2025-12-21 06:18:04 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,35 +1,30 @@
 if __name__ == "__main__":
     pass
 
-#а) Имя файла
+import datetime
+
+#а)
 name = input('Введите имя файла: ')
 bad_symb = '\\/*?:"<>|'
 if any(symb in name for symb in bad_symb):
-    print('Недопустимое имя файла')
+    raise Exception('Недопустимое имя файла')
 else:
     print('Имя файла введено корректно')
 
-#б) Дата рождения
+#б)
 date = input('Введите дату рождения (дд.мм.гггг): ')
-a = date.split('.')
-if len(a) == 3 and len(a[0]) == 2 and len(a[1]) == 2 and len(a[2]) == 4:
+try:
+    datetime.datetime.strptime(date, "%d.%m.%Y")
     print('Дата введена корректно')
-else:
-    print('Неверный формат даты')
+except ValueError:
+    raise Exception('Неверный формат даты')
 
-# в) Координаты
+#в)
 xy = input('Введите координаты (x y): ').split()
-
 if len(xy) != 2:
-    print('Некорректные координаты')
-else:
-    x, y = xy
-    if (x.replace('-', '').replace('.', '').isdigit() and 
-        y.replace('-', '').replace('.', '').isdigit() and
-        x.count('-') <= 1 and y.count('-') <= 1 and
-        x.count('.') <= 1 and y.count('.') <= 1 and
-        (x[0] == '-' if '-' in x else True) and
-        (y[0] == '-' if '-' in y else True)):
-        print('Координаты введены корректно')
-    else:
-        print('Некорректные координаты')
+    raise Exception('Некорректные координаты')
+try:
+    x, y = float(xy[0]), float(xy[1])
+    print('Координаты введены корректно')
+except ValueError:
+    raise Exception('Некорректные координаты')
