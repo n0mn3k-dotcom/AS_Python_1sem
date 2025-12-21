e44a0555ccf7f3dde16e34a3af09cda56548dba3
0y0y0y <n0mn3k@gmail.com>
2025-12-21 05:05:21 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,16 @@
 if __name__ == "__main__":
     pass
+
+with open("f.txt", "r", encoding="utf-8") as f:
+    numbers = [float(line.strip()) for line in f if line.strip() != ""]
+
+
+min_num = min(numbers)
+max_num = max(numbers)
+
+
+sum_min_max = min_num + max_num
+
+print("Минимальное число:", min_num)
+print("Максимальное число:", max_num)
+print("Сумма минимума и максимума:", sum_min_max)
