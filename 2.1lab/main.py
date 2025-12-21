7fb4b09b98edfd757e6d35d8e770cb093a96ba2d
0y0y0y <n0mn3k@gmail.com>
2025-12-21 03:27:24 +0300

    Update main.py

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,14 @@
 if __name__ == "__main__":
     pass
+
+def check_keys(d, keys):
+    for k in keys:
+        if k not in d:
+            return False
+    return True
+
+
+d = {1: 2, 3: 4}
+keys = [1, 3]
+
+print(check_keys(d, keys))
