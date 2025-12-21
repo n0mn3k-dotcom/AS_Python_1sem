9a77098486ca78233ea72e10f82ebafa735e34be
0y0y0y <n0mn3k@gmail.com>
2025-12-21 04:08:52 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -1,2 +1,16 @@
 if __name__ == "__main__":
     pass
+
+import line_module as lm
+
+
+eq1 = lm.line_equation(1, 2, 3, 6)
+print("Уравнение прямой через (1,2) и (3,6):", eq1)
+
+
+parallel = lm.are_parallel(2, 1, 2, 3)
+print("Прямые параллельны?", parallel)
+
+
+on_line = lm.is_point_on_line(2, 5, 2, 1)
+print("Точка (2,5) на прямой y=2*x+1?", on_line)
