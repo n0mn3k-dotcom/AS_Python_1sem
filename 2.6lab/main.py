ba4e720748db51e4ceb60af0f4596c4ad4d3cedd
0y0y0y <n0mn3k@gmail.com>
2025-12-21 05:58:55 +0300

    Update main.py

--- 2.6lab/main.py
+++ 2.6lab/main.py
@@ -3,14 +3,22 @@ if __name__ == "__main__":
 
 import line_module as lm
 
+k1, b1 = lm.line_through_points(1, 2, 3, 6)
+if k1 is None:
+    print(f"Прямая через (1,2) и (3,6): x = {b1}")
+else:
+    print(f"Прямая через (1,2) и (3,6): y = {k1}*x + {b1}")
 
-eq1 = lm.line_equation(1, 2, 3, 6)
-print("Уравнение прямой через (1,2) и (3,6):", eq1)
+k2, b2 = lm.line_through_points(0, 0, 2, 4)
+if k2 is None:
+    print(f"Прямая через (0,0) и (2,4): x = {b2}")
+else:
+    print(f"Прямая через (0,0) и (2,4): y = {k2}*x + {b2}")
 
+print("Прямые параллельны?", lm.are_parallel(k1, k2))
 
-parallel = lm.are_parallel(2, 1, 2, 3)
-print("Прямые параллельны?", parallel)
+x, y = 2, 5
+print(f"Точка ({x},{y}) на первой прямой?", lm.point_on_line(k1, b1, x, y))
 
-
-on_line = lm.is_point_on_line(2, 5, 2, 1)
-print("Точка (2,5) на прямой y=2*x+1?", on_line)
+x2, y2 = 2, 4
+print(f"Точка ({x2},{y2}) на первой прямой?", lm.point_on_line(k1, b1, x2, y2))
