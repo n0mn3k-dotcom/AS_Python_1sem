50e2dccd103609cae641e64d31a47de675e34d60
0y0y0y <n0mn3k@gmail.com>
2025-12-21 03:41:30 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,19 @@
 if __name__ == "__main__":
     pass
+
+
+import math
+
+def RingS(R1, R2):
+    if R1 < R2:
+        R1, R2 = R2, R1
+
+    s = math.pi * (R1 * R1 - R2 * R2)
+    return s
+
+
+r1 = float(input("Введите R1: "))
+r2 = float(input("Введите R2: "))
+
+result = RingS(r1, r2)
+print("Площадь =", result)
