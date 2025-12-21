f0ed25c6deaf56073621c1b167b8a7308d7491fc
0y0y0y <n0mn3k@gmail.com>
2025-12-21 03:55:48 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,21 @@
 if __name__ == "__main__":
     pass
+
+
+
+calls = 0
+
+def FibRec(n):
+    global calls
+    calls += 1
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return FibRec(n-1) + FibRec(n-2)
+
+numbers = [5, 6, 7, 8, 9]
+
+for num in numbers:
+    calls = 0
+    f = FibRec(num)
+    print(f"Fib({num}) = {f}, calls = {calls}")
