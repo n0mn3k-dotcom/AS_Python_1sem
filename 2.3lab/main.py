6af8faa0bfde3bab3d141ddd6efece01fe7eb11b
0y0y0y <n0mn3k@gmail.com>
2025-12-21 03:45:34 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,15 @@
 if __name__ == "__main__":
     pass
+
+def Transp(A):
+    return list(map(list, zip(*A)))
+
+
+m = int(input("Введите размер матрицы: "))
+
+A = [list(map(int, input().split())) for _ in range(m)]
+
+T = Transp(A)
+
+for row in T:
+    print(*row)
