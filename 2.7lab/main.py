db03eabcce8b6e0050db150019bc3b907968be11
0y0y0y <n0mn3k@gmail.com>
2025-12-21 04:54:56 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,24 @@
 if __name__ == "__main__":
     pass
+
+with open("file1.txt", "r", encoding="utf-8") as f1:
+    lines1 = f1.readlines()
+
+with open("file2.txt", "r", encoding="utf-8") as f2:
+    lines2 = f2.readlines()
+
+new_lines = []
+for i in range(len(lines1)):
+    line1 = lines1[i].rstrip("\n")
+    if i < len(lines2):
+        line2 = lines2[i].rstrip("\n")
+        new_lines.append(line1 + line2 + "\n")
+    else:
+        new_lines.append(line1 + "\n")
+
+with open("file1.txt", "w", encoding="utf-8") as f1:
+    f1.writelines(new_lines)
+
+print("Файлы объединены: ")
+for line in new_lines:
+    print(line, end="")
