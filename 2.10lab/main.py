a1fd898d3c5c395f771c77f8ef587d0dbffa119c
0y0y0y <n0mn3k@gmail.com>
2025-12-21 06:21:39 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -7,7 +7,7 @@ import datetime
 name = input('Введите имя файла: ')
 bad_symb = '\\/*?:"<>|'
 if any(symb in name for symb in bad_symb):
-    raise Exception('Недопустимое имя файла')
+    print('Недопустимое имя файла')
 else:
     print('Имя файла введено корректно')
 
@@ -17,14 +17,15 @@ try:
     datetime.datetime.strptime(date, "%d.%m.%Y")
     print('Дата введена корректно')
 except ValueError:
-    raise Exception('Неверный формат даты')
+    print('Неверный формат даты')
 
 #в)
 xy = input('Введите координаты (x y): ').split()
 if len(xy) != 2:
-    raise Exception('Некорректные координаты')
-try:
-    x, y = float(xy[0]), float(xy[1])
-    print('Координаты введены корректно')
-except ValueError:
-    raise Exception('Некорректные координаты')
+    print('Некорректные координаты')
+else:
+    try:
+        x, y = float(xy[0]), float(xy[1])
+        print('Координаты введены корректно')
+    except ValueError:
+        print('Некорректные координаты')
