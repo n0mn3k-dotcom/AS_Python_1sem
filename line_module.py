4a21399b05b5577a3f80b8b972f3f3f2c56c61c5
0y0y0y <n0mn3k@gmail.com>
2025-12-21 05:56:34 +0300

    Update line_module.py

--- line_module.py
+++ line_module.py
@@ -1,12 +1,18 @@
-def line_equation(x1, y1, x2, y2):
+def line_through_points(x1, y1, x2, y2):
     if x2 - x1 == 0:
-        return "x = " + str(x1)
+        return None, x1
     k = (y2 - y1) / (x2 - x1)
-    b = y1 - k * x1
-    return f"y = {k}*x + {b}"
+    b = y1 - k*x1
+    return k, b
 
-def are_parallel(k1, b1, k2, b2):
+def are_parallel(k1, k2):
+    if k1 is None and k2 is None:
+        return True
+    if k1 is None or k2 is None:
+        return False
     return k1 == k2
 
-def is_point_on_line(x0, y0, k, b):
-    return y0 == k * x0 + b
+def point_on_line(k, b, x, y):
+    if k is None:
+        return x == b
+    return y == k*x + b
